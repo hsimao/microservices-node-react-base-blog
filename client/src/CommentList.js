c6e76144b066c09a556f18f36e001fe641d90633
hsimao <e087754958@gmail.com>
2020-05-03 17:47:42 +0800

    init

---
+++ client/src/CommentList.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default ({ comments }) => {
+  const renderComments = comments.map(comment => <li key={comment.id}>{comment.content}</li>)
+  return <ul>{renderComments}</ul>
+}
