002603559f408928c569b4f10b6177392ee35174
hsimao <e087754958@gmail.com>
2020-05-06 20:21:45 +0800

    feat: client 新增留言狀態判斷

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
 export default ({ comments }) => {
-  const renderComments = comments.map(comment => <li key={comment.id}>{comment.content}</li>)
+  const renderComments = comments.map(comment => {
+    let content
+
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content
+        break
+      case 'rejected':
+        content = 'This comment has been rejected'
+        break
+      case 'pending':
+        content = 'This comment is awaiting moderation'
+        break
+      default:
+        content = 'This comment is awaiting moderation'
+    }
+
+    return <li key={comment.id}>{content}</li>
+  })
   return <ul>{renderComments}</ul>
 }
