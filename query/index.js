4d8444477c3c42b92a69fd076b2158f3153b67b7
hsimao <e087754958@gmail.com>
2020-05-06 20:53:46 +0800

    feat: 解決 query service 中斷重啟時數據遺失問題

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParse = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParse.json())
@@ -23,14 +24,7 @@ const posts = {}
 }
 */
 
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
-
-// 接收到對應 events 時, 將後續需要 query 的資料儲存起來
-app.post('/events', (req, res) => {
-  const { type, data } = req.body
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data
     posts[id] = { id, title, comments: [] }
@@ -49,12 +43,30 @@ app.post('/events', (req, res) => {
     comment.status = status
     comment.content = content
   }
+}
 
-  console.log(posts)
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+// 接收到對應 events 時, 將後續需要 query 的資料儲存起來
+app.post('/events', (req, res) => {
+  const { type, data } = req.body
+
+  handleEvent(type, data)
 
   res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Query Listening on 4002')
+
+  // 每次從啟 query service 時跟 event service 取得當前所有 events
+  // 並逐一執行將資料補回
+  const res = await axios.get('http://localhost:4005/events')
+  for (let event of res.data) {
+    console.log('Processing event: ', event.type)
+
+    handleEvent(event.type, event.data)
+  }
 })
